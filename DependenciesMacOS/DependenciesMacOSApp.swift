3fb6b931d740f5e36e90e49762c5f180fea068a8
Kushagra3125 <sharmakushagra3125@gmail.com>
2026-04-23 17:46:09 +0530

    Initial Commit

---
+++ DependenciesMacOS/DependenciesMacOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  DependenciesMacOSApp.swift
+//  DependenciesMacOS
+//
+//  Created by Kushagra Sharma on 23/04/26.
+//
+
+import SwiftUI
+
+@main
+struct DependenciesMacOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
